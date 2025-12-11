9cb75c949e153a1a814946c9a483f52fdf4441aa
احمد خالد احمد العمودي <2003alamoudi@gmail.com>
2025-12-11 10:14:01 +0300

    Add bottomNavigationBar project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:random_string/random_string.dart';
-import 'package:tryingfirebase/dbfirebase.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -24,7 +16,7 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
+        // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
@@ -64,12 +56,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var _formKey = GlobalKey<FormState>();
-  TextEditingController lecName=new TextEditingController();
-  TextEditingController lecPhNo=new TextEditingController();
-  TextEditingController lecCol=new TextEditingController();
-  TextEditingController lecDept=new TextEditingController();
-  Databasemethods dbobj=new Databasemethods();
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -118,136 +105,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "إضافة منتجات للفاتورة ",
-                    style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "إضافة منتجات للفاتورة ",
-                    style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                  ),
-                  //styling
-                  TextFormField(
-                    controller: lecName,
-                    decoration: InputDecoration(labelText: 'اسم المحاضر'),
-                    keyboardType: TextInputType.emailAddress,
-                    onFieldSubmitted: (value) {
-
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter a valid email!';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: lecCol,
-                    decoration: InputDecoration(labelText: 'كلية'),
-                    keyboardType: TextInputType.emailAddress,
-                    onFieldSubmitted: (value) {
-                      //Validator
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter a valid email!';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: lecDept,
-                    decoration: InputDecoration(labelText: 'مدرس بقسم'),
-                    keyboardType: TextInputType.emailAddress,
-                    onFieldSubmitted: (value) {
-                      //Validator
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty)
-                      {
-                        return 'Enter a valid email!';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: lecPhNo,
-                    decoration: InputDecoration(labelText: 'رقم الهاتف'),
-                    keyboardType: TextInputType.emailAddress,
-                    onFieldSubmitted: (value) {
-                      //Validator
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty)
-                      {
-                        return 'Enter a valid email!';
-                      }
-                      return null;
-                    },
-                  ),
-                  //box styling
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width * 0.1,
-                  ),
-                  //text input
-
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width * 0.1,
-                  ),
-                  FittedBox(
-                    child: Row(
-                      children: [
-                        ElevatedButton(
-
-                            child: Text(
-                              "تسجيل",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                              ),
-                            ),
-                            onPressed: () async{
-
-                              String id =randomAlphaNumeric(10);
-                              // _submit();
-                              Map<String, dynamic> lecInfoMap={
-                                "lecName":lecName.text,
-                                "ID":id,
-                                "lecCol":lecCol.text,
-                                "lecDept":lecDept.text,
-                                "lecPhNo":lecPhNo.text,
-                              };
-                              await Databasemethods().addData(lecInfoMap, id).then((value) {
-                                Fluttertoast.showToast(
-                                    msg: "Lecturer infor have been added successfully ",
-                                    toastLength: Toast.LENGTH_SHORT,
-                                    gravity: ToastGravity.CENTER,
-                                    timeInSecForIosWeb: 1,
-                                    backgroundColor: Colors.red,
-                                    textColor: Colors.white,
-                                    fontSize: 16.0
-                                );
-                              });
-                              //await dbobj.addData(LecInfoMap, id)
-
-                              //costname.text="";
-                              lecName.text="";
-                              lecCol.text="";
-                              lecDept.text="";
-                              lecPhNo.text="";
-
-                            }
-                        ),
-
-
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
