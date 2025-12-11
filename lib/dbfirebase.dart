b2dd4568695904a7dc55085e1f71eb1418573ea1
احمد خالد احمد العمودي <2003alamoudi@gmail.com>
2025-12-11 10:06:20 +0300

    Add bottomNavigationBar project

---
+++ lib/dbfirebase.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Databasemethods{
+Future addData(Map<String, dynamic> LecInfoMap, String id)async{
+return await FirebaseFirestore.instance
+    .collection("lecturer")
+    .doc(id)
+    .set(LecInfoMap);
+}
